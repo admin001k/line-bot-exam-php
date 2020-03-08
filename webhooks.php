bea192de4a23d456b9702662fa656ee33c6ce2b2
admin001k <61932599+admin001k@users.noreply.github.com>
2020-03-08 16:49:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'urHOZRlEz9vIZErQ8ISRdcBdl7r8WvsqbKX+oGEyDQ+zd8U+oj+OgYxERX7gbhjJiAk0F2nEFjQUVHuHp4FKOhCl+tXJ8l1bCSu+WArMQGe1Mc4PxABqDY+EsrwExrxa2pAyf5Y9oFmsihcCvPYHfAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
